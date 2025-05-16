ab731e3f0ee6e801e385eca64bffc88ca32ab8ad
milosanchezdespres <milosanchezdespres@gmail.com>
2025-05-16 19:38:05 +0200

    removing factories

--- YoBattle/main.cpp
+++ YoBattle/main.cpp
@@ -1,5 +1,4 @@
 #include "ECS/Seri.h"
-#include "ECS/Factory.h"
 using namespace ECS;
 
 struct CustomSeri : public Seri
@@ -27,31 +26,12 @@ struct CustomSeri : public Seri
     }
 };
 
-class CustomFactory : public Factory
-{
-    public:
-        CustomFactory() : Factory() {}
-
-        Seri* OnExtraFactor(json json_data) override
-        {
-            if (json_data["type"] == "CustomSeri") { return (new CustomSeri())->load(json_data); }
-
-            return __default;
-        }
-};
-
 int main()
 {
-    CustomFactory* factory = new CustomFactory();
-
     CustomSeri* test = new CustomSeri();
     test->load("debug/test", false);
 
-    auto* test_copy = factory->factor(TO_JSON(test));
-
     cout << test << endl;
-    cout << "=============" << endl;
-    cout << test_copy << endl;
 
     return 0;
 }
