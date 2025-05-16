cbb107a48fb4c625584f86fa03b24979a52a5833
milosanchezdespres <milosanchezdespres@gmail.com>
2025-05-16 23:36:11 +0200

    reset project

--- YoBattle/main.cpp
+++ YoBattle/main.cpp
@@ -1,37 +1,6 @@
-#include "ECS/Seri.h"
-using namespace ECS;
-
-struct CustomSeri : public Seri
-{
-    float x, y;
-
-    CustomSeri() : Seri()
-    {
-        x = 0;
-        y = 0;
-
-        __OnInit();
-    }
-
-    void __OnJson()
-    {
-        __json["x"] = x;
-        __json["y"] = y;
-    }
-
-    void __OnLoad()
-    {
-        x = __json["x"];
-        y = __json["y"];
-    }
-};
-
 int main()
 {
-    CustomSeri* test = new CustomSeri();
-    test->load("debug/test", false);
-
-    cout << test << endl;
+    //...
 
     return 0;
 }
