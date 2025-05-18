cce3224b99dc8132071e59de1b23c1ae0de40c89
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-18 17:07:57 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 600, "YoBattle");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Welcome to YoBattle!", 220, 280, 30, RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
