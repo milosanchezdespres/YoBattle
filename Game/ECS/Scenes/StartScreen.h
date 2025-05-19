f1ef7cb4a97c008f875e0d01723bad734411abf1
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-19 13:04:31 +0200

    displaying a map

--- Game/ECS/Scenes/StartScreen.h
+++ Game/ECS/Scenes/StartScreen.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "../../../ECS/ECS.h"
+#include "../../../MAP/MAP.h"
+using namespace U8INT_MAP;
+
 #include "../Systems/SpriteRenderSystem.h"
 #include "../Entities/Entity2D.h"
 
@@ -8,58 +11,54 @@ namespace YOBATTLE
 {
     struct StartScreen : public Scene
     {
+        bool map_refresh;
+        metatile test_map[2500] = {0};
+        RenderTexture2D test_map_surface;
+
         StartScreen() : Scene() {}
 
         void OnEnter() override
         {
-            Game::instance().load_texture("test");
+            Game::instance().load_texture("tileset");
+            Game::instance().load_texture("player");
 
             attach<SpriteRenderSystem>();
 
-            add<Entity2D>("test");
-            get("test")->get<Sprite>("body")->texture = "test";
-            get("test")->get<Sprite>("body")->position.x = 130;
-            get("test")->get<Sprite>("body")->position.y = 130;
-            get("test")->get<Sprite>("body")->tile_index = 1;
-            get("test")->get<Sprite>("body")->tile_size = 128;
+            add<Entity2D>("player");
+            get("player")->get<Sprite>("body")->texture = "player";
+            get("player")->get<Sprite>("body")->position.x = 130;
+            get("player")->get<Sprite>("body")->position.y = 130;
+            get("player")->get<Sprite>("body")->tile_index = 1;
+            get("player")->get<Sprite>("body")->tile_size = 16;
+            get("player")->get<Sprite>("body")->scale = 5;
 
-            add<Entity2D>("test2");
-            get("test2")->get<Sprite>("body")->texture = "test";
-            get("test2")->get<Sprite>("body")->position.x = 243;
-            get("test2")->get<Sprite>("body")->position.y = 243;
-            get("test2")->get<Sprite>("body")->tile_index = 2;
-            get("test2")->get<Sprite>("body")->tile_size = 128;
+            sys<SpriteRenderSystem>()->upload(get("player")->get<Sprite>("body"));
 
-            add<Entity2D>("test3");
-            get("test3")->get<Sprite>("body")->texture = "test";
-            get("test3")->get<Sprite>("body")->position.x = 450;
-            get("test3")->get<Sprite>("body")->position.y = 300;
-            get("test3")->get<Sprite>("body")->width = 350;
-            get("test3")->get<Sprite>("body")->height = 350;
-
-            add<Entity2D>("test4");
-            get("test4")->get<Sprite>("body")->texture = "test";
-            get("test4")->get<Sprite>("body")->position.x = 780;
-            get("test4")->get<Sprite>("body")->position.y = 178;
-
-            sys<SpriteRenderSystem>()->upload(get("test")->get<Sprite>("body"));
-            sys<SpriteRenderSystem>()->upload(get("test2")->get<Sprite>("body"));
-            sys<SpriteRenderSystem>()->upload(get("test3")->get<Sprite>("body"));
-            sys<SpriteRenderSystem>()->upload(get("test4")->get<Sprite>("body"));
-
-            //...
+            vector<metatile> new_map = map_make(50, 50);
+            for (int i = 0; i < 100; ++i) { test_map[i] = new_map[i]; }
+            map_refresh = true;
         }
  
         void OnDraw() override
         {
-            sys<SpriteRenderSystem>()->update(0);
-
             //...
+
+            if (map_refresh)
+            {
+                test_map_surface = map_draw("tileset", test_map, 8, 50, 50);
+                map_refresh = false;
+            }
+
+            //for test purposes, later, store a buffer for rect, position
+            DrawTexturePro(test_map_surface.texture, {0, 0, (float)test_map_surface.texture.width, -(float)test_map_surface.texture.height}, {0, 0, (float)test_map_surface.texture.width * 5, (float)test_map_surface.texture.height * 5}, {0, 0}, 0.0f, WHITE);
+        
+            sys<SpriteRenderSystem>()->update(0);
         }
 
         void OnExit() override
         {
-            Game::instance().unload_texture("test");
+            Game::instance().unload_texture("tileset");
+            Game::instance().unload_texture("player");
 
             //...
         }
--- MAP/MAP.h
+++ MAP/MAP.h
@@ -1,4 +1,90 @@
 #pragma once
 
-//...
+#include <raylib.h>
+
+#include <cstdint>
+#include <string>
+using namespace std;
+
+#include "../WINDOW/WINDOW.h"
+using namespace RAYWIN;
+
+namespace U8INT_MAP
+{
+    struct metatile
+    {
+        uint8_t top_left;
+        uint8_t top_right;
+        uint8_t bottom_left;
+        uint8_t bottom_right;
+        uint8_t collision_type;
+
+        metatile()
+        {
+            top_left = 0;
+            top_right = 0;
+            bottom_left = 0;
+            bottom_right = 0;
+            collision_type = 0;
+        }
+
+        metatile(uint8_t index)
+        {
+            top_left = index;
+            top_right = index;
+            bottom_left = index;
+            bottom_right = index;
+            collision_type = 0;
+        }
+    };
+
+    inline vector<metatile> map_make(int width, int height)
+    {
+        vector<metatile> tiles;
+
+        for(int i = 0; i < width * height; i++) { tiles.push_back(metatile()); }
+
+        return tiles;
+    }
+
+    inline Rectangle tile_rect_make(string texture, uint8_t tileID)
+    {
+        int tileWidth = 8;
+        int tileHeight = 8;
+        int tilesPerRow = Game::instance().texture(texture).width / tileWidth;
+
+        int tx = tileID % tilesPerRow;
+        int ty = tileID / tilesPerRow;
+
+        return Rectangle{ float(tx * tileWidth), float(ty * tileHeight), float(tileWidth), float(tileHeight) };
+    }
+
+    inline RenderTexture2D map_draw(string texture, auto map, int tile_size, int width, int height)
+    {
+        RenderTexture2D tilemapTexture = LoadRenderTexture(width * tile_size, height * tile_size);
+
+        BeginTextureMode(tilemapTexture);
+        ClearBackground(BLANK);
+
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x) 
+            {
+                metatile meta = map[y * width + x];
+
+                int px = x * 16;
+                int py = y * 16;
+
+                DrawTextureRec(Game::instance().texture(texture), tile_rect_make(texture, meta.top_left),  { (float)(px),     (float)(py)     }, WHITE);
+                DrawTextureRec(Game::instance().texture(texture), tile_rect_make(texture, meta.top_right),    { (float)(px + 8), (float)(py)     }, WHITE);
+                DrawTextureRec(Game::instance().texture(texture), tile_rect_make(texture, meta.bottom_left),  { (float)(px),     (float)(py + 8) }, WHITE);
+                DrawTextureRec(Game::instance().texture(texture), tile_rect_make(texture, meta.bottom_right), { (float)(px + 8), (float)(py + 8) }, WHITE);
+            }
+        }
+
+        EndTextureMode();
+
+        return tilemapTexture;
+    }
+}
 
