2cba6e7bc46e292c89805bde5cdf1193eda96d4e
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-20 18:34:15 +0200

    axis

--- Game/Systems/ControllerSystem.h
+++ Game/Systems/ControllerSystem.h
@@ -31,22 +31,22 @@ namespace YoBattleGame
                 {
                     if(Game::instance().is_down("up"))
                     {
-                        //...
+                        owner->get<StateMachine>("states")->axis = 0;
                         owner->get<StateMachine>("states")->does<PlayerWalk>();
                     }
                     else if(Game::instance().is_down("down"))
                     {
-                        //...
+                        owner->get<StateMachine>("states")->axis = 1;
                         owner->get<StateMachine>("states")->does<PlayerWalk>();
                     }
                     else if(Game::instance().is_down("left"))
                     {
-                        //...
+                        owner->get<StateMachine>("states")->axis = 2;
                         owner->get<StateMachine>("states")->does<PlayerWalk>();
                     }
                     else if(Game::instance().is_down("right"))
                     {
-                        //...
+                        owner->get<StateMachine>("states")->axis = 3;
                         owner->get<StateMachine>("states")->does<PlayerWalk>();
                     }
                     else { owner->get<StateMachine>("states")->does<PlayerIdle>(); }
