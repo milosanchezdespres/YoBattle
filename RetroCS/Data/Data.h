31536f4199c8d92c1c51b834d2aa7c26f09419fb
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-21 10:59:20 +0200

    saving objects

--- RetroCS/Data/Data.h
+++ RetroCS/Data/Data.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../Tools/Externals.h"
+
 #include "nlohman_json.h"
 using json = nlohmann::json;
 
@@ -31,20 +33,63 @@ namespace RetroCS
             virtual void OnJsonRead() {}
         };
 
-        template <typename T, typename = enable_if_t<is_base_of<DataObject, T>::value>>
-        inline void save_to(T* data_object, string path)
+        inline string file_content(string filename, string folder = "", string ext = "")
+        {
+            string path = filename;
+            if(ext != "") { path += "." + ext; }
+            if(folder != "") { path = folder + "/" + path; }
+
+            ifstream file(path);
+
+            if(!file) { throw runtime_error("Failed to open file for writing: " + path); }
+            else
+            {
+                stringstream buffer;
+
+                buffer << file.rdbuf();
+
+                file.close();
+
+                return buffer.str();
+            }
+        }
+
+        inline void save_file_content(string content, string filename, string folder = "", string ext = "")
+        {
+            string path = filename;
+            if(ext != "") { path += "." + ext; }
+            if(folder != "") { path = folder + "/" + path; }
+
+            fs::create_directories(folder);
+
+            ofstream file(path, ios::trunc);
+
+            if(!file) { throw runtime_error("Failed to open file for writing: " + path); }
+            else
+            {
+                file << content;
+                file.close();
+            }
+        }
+
+        inline void save_to(DataObject* data_object, string filename, string _folder = "")
         {
             json _json = data_object->to_json();
 
-            //...
+            string folder = _folder;
+            if(folder != "") folder = "/" + folder;
+            folder = "Game/data" + folder;
+
+            save_file_content(_json.dump(4), filename, folder, "json");
         }
 
-        template <typename T, typename = enable_if_t<is_base_of<DataObject, T>::value>>
-        inline void load_from(T* data_object, string path)
+        inline void load_from(DataObject* data_object, string filename, string _folder = "")
         {
-            json _json = {};
+            string folder = _folder;
+            if(folder != "") folder = "/" + folder;
+            folder = "Game/data" + folder;
 
-            //...
+            json _json = json::parse(file_content(filename, folder, "json"));
 
             data_object->from_json(_json);
         }
--- RetroCS/Tools/Externals.h
+++ RetroCS/Tools/Externals.h
@@ -11,4 +11,9 @@
 #include <unordered_map>
 #include <algorithm>
 
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
 using namespace std;
+namespace fs = filesystem;
