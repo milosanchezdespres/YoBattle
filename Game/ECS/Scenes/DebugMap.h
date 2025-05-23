3682a2964dc7e42c67b576163d0dab3f42be0947
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-23 12:04:05 +0200

    camera

--- Game/ECS/Scenes/DebugMap.h
+++ Game/ECS/Scenes/DebugMap.h
@@ -30,33 +30,12 @@ namespace YoKard
                 attach<AnimationSystem>();
                 //...
 
-                //add<Player>();
-                //get<Player>()->get<Image>()->x = 0;
-                //get<Player>()->get<Image>()->y = 0;
+                add<Player>();
+                get<Player>()->get<Image>()->x = 50;
+                get<Player>()->get<Image>()->y = 50;
 
                 camera().set_zoom(8);
-                //camera().follow(get<Player>()->get<Image>()->x, get<Player>()->get<Image>()->y);
-///*
-                int playersPerRow = 24;
-                int totalPlayers = 10000;//THIS WORKS AT 30FPS
-                int spacingX = 25;
-                int spacingY = 25; // Adjust vertical spacing as needed
-                int startY = 0;
-
-                for (int i = 0; i < totalPlayers; i++)
-                {
-                    add<Player>("player" + to_string(i));
-                    auto image = get<Player>("player" + to_string(i))->get<Image>();
-                    //image->scale = 5;
-
-                    // Calculate x and y for grid layout
-                    int col = i % playersPerRow;  // column number (0 to 7)
-                    int row = i / playersPerRow;  // row number
-
-                    image->x = col * spacingX;
-                    image->y = startY + row * spacingY;
-                }
-//*/
+                camera().follow(get<Player>()->get<Image>()->x, get<Player>()->get<Image>()->y);
             }
 
             void OnExit() override { }
