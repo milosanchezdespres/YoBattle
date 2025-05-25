7c8a7892fa9ea3159862bde47d35cfd39199aaa6
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-25 14:45:35 +0200

    cleaned project for refactorisation

--- RetroCS/Utils/Utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "includes/ext_includes.h"
-#include "includes/inlines.h"
-#include "includes/object.h"
-#include "includes/box.h"
-//...
-
-using namespace retrocs::utils;
--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include "RetroCS/RetroCS.h"
-#include "Game/scenes/DebugScene.h"
-
 int main()
 {
-    hub::init<DebugScene>("YoKard");
-
-    hub::update();
+    //...
 
     return 0;
 }
