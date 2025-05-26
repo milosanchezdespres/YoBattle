90638456851285c1de5ee5d6cd35d1b4c5bebbfc
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-26 22:37:16 +0200

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,22 +1,8 @@
-#include "retrocs/retrocs.h"
-#include "Game/scenes/DebugMap.h"
-
-using namespace game;
-using namespace game::ecs;
+//...
 
 int main()
 {
-    DebugMap scene = DebugMap();
-    VirtualScreen& screen = scene.screen_space;
-
-    scene.enter();
-
-    scene.update();
-
-    debug(scene.get_at<Entity>(7, 2).id);
-
-    scene.exit();
-
+    //...
 
     return 0;
 }
