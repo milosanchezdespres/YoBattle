4a2869c389ee8872fa9a9cdbad44c013c2f7d6f1
milosanchezdespres <milosanchezdespres@mail.com>
2025-05-26 21:38:51 +0200

    ncm

--- main.cpp
+++ main.cpp
@@ -3,19 +3,30 @@
 int main()
 {
     Scene scene;
+    VirtualScreen& screen = scene.screen_space;
 
     Entity entity1 = scene.spawn<Entity>({7, 2});
     Entity entity2 = scene.spawn<Entity>({4, 8});
-    Entity entity6 = scene.spawn<Entity>({1, 1});
+    Entity entity3 = scene.spawn<Entity>({1, 1});
 
     scene.remove_at(4, 8);
 
-    for(cell _cell : scene)
+    for(int i = 0; i < 120; i++)
     {
+        entity3.place(screen, entity3.transform().x() + 4000 * 1.16, entity3.transform().y() + 400 * 1.16);
+    }
+
+    for (int id : screen.in_bounds_ids)
+    {
+        cell _cell = screen.tile(id);
         auto entity = scene.get_at<Entity>(_cell);
 
+        debug(entity.id);
+        debug(entity.transform().i());
+        debug(entity.transform().j());
         debug(entity.transform().x());
         debug(entity.transform().y());
+        debug(".......................................");
     }
 
     return 0;
--- retrocs/ecs/Entity.h
+++ retrocs/ecs/Entity.h
@@ -13,6 +13,17 @@ namespace retrocs
 
             Entity(int _id) : View<Entity>(_id) { transform_view = Transform(); transform_view.id = id; }
 
+            void place(VirtualScreen& screen_space, float x, float y)
+            {
+                transform_view.x() = x;
+                transform_view.y() = y;
+
+                int new_i = TO_CELL(x, y).i;
+                int new_j = TO_CELL(x, y).j;
+
+                if (transform_view.coordinates) { screen_space.update(id, {new_i, new_j}); }
+            }
+
             bool _on_init(VirtualScreen& screen_space) { return true; }
 
             void populate_data(VirtualScreen& screen_space)
